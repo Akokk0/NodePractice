d129ae8b148a58de818f941e6187e452df15b8b8
akokko <zhminecraft@sina.com>
2022-08-14 23:28:21 +0800

    update vue3 data proxy and reflect

--- 15Class.js
+++ 15Class.js
@@ -3,7 +3,7 @@ let person = {
     age: 20
 }
 
-let p = {}
+/*let p = {}
 // Vue2中的响应式
 const keys = Object.keys(person)
 keys.forEach(item => {
@@ -26,27 +26,34 @@ console.log(p.age)
 p.name = 'ls'
 p.age = 21
 
-console.log(person)
+console.log(person)*/
 
 /*console.log(person['name'])*/
 
 // Vue3中的响应式
 
-/*
 const p = new Proxy(person, {
+    // CRUD
+    // 读取
     get(target, propName) {
         console.log(`有人读取了p身上的${propName}属性`)
-        return target[propName]
+        return Reflect.get(target, propName)
     },
+    // 修改或追加
     set(target, propName, value) {
         console.log(`有人修改了p身上的${propName}属性，我要去更新界面了！`)
-        target[propName] = value
+        Reflect.set(target, propName, value)
+    },
+    // 删除
+    deleteProperty(target, propName) {
+        console.log(`有人删除了p身上的${propName}属性`)
+        return Reflect.deleteProperty(target, propName)
     }
 })
 
 console.log(person)
 
 p.name = 'ls'
+delete p.name
 
 console.log(person)
-*/
